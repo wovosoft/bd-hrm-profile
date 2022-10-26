b9846a387409058d972ec4ca007ce02bac381790
Narayan Adhikary <wovosoft@gmail.com>
2022-10-26 14:05:11 +0600

    Added Address Relations

--- src/Models/PermanentAddress.php
+++ src/Models/PermanentAddress.php
@@ -4,14 +4,16 @@ namespace Wovosoft\BdHrmProfile\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Scout\Searchable;
 use Wovosoft\BdHrmProfile\Interfaces\HasRulesInterface;
+use Wovosoft\BdHrmProfile\Traits\HasAddressRelations;
 use Wovosoft\BdHrmProfile\Traits\HasProfile;
 use Wovosoft\BdHrmProfile\Traits\HasTablePrefix;
 
 class PermanentAddress extends Model implements HasRulesInterface
 {
-    use HasFactory, HasProfile, HasTablePrefix, Searchable;
+    use HasFactory, HasProfile, HasTablePrefix, Searchable, HasAddressRelations;
 
     public static function rules(): array
     {
@@ -29,4 +31,5 @@ class PermanentAddress extends Model implements HasRulesInterface
             "email" => ["email", "nullable"],
         ];
     }
+
 }
--- src/Models/PresentAddress.php
+++ src/Models/PresentAddress.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 use Wovosoft\BdHrmProfile\Interfaces\HasRulesInterface;
+use Wovosoft\BdHrmProfile\Traits\HasAddressRelations;
 use Wovosoft\BdHrmProfile\Traits\HasProfile;
 use Wovosoft\BdHrmProfile\Traits\HasTablePrefix;
 
 class PresentAddress extends Model implements HasRulesInterface
 {
-    use HasFactory, HasProfile, HasTablePrefix, Searchable;
+    use HasFactory, HasProfile, HasTablePrefix, Searchable, HasAddressRelations;
 
     public static function rules(): array
     {
